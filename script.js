0932ca6fc2191611fb07d849a4f1d6884d9d38b6
azim218 <138890569+azim218@users.noreply.github.com>
2025-08-31 00:03:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Простейшие интерактивности
+document.querySelectorAll('.faq-item h3').forEach(title => {
+    title.addEventListener('click', () => {
+        const p = title.nextElementSibling;
+        if (p.style.display === 'block') p.style.display = 'none';
+        else p.style.display = 'block';
+    });
+});
